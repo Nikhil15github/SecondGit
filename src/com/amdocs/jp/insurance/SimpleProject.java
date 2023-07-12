2bbc2b5d677df73c346ca21efa8528530c13b55b
Nikhil <nikpawarnp2@gmail.com>
2023-07-12 11:21:17 +0530

    Second Project

---
+++ src/com/amdocs/jp/insurance/SimpleProject.java
@@ -0,0 +1,14 @@
+package com.amdocs.jp.insurance;
+
+public class SimpleProject 
+{
+
+	public static void main(String[] args) {
+		
+		
+		String name= "Nikhil Pawar";
+		
+		System.out.println(name);
+	}
+	
+}
