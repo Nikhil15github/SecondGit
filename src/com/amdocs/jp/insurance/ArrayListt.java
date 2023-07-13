f7e4aab5fa39467dc067f109ba6b5ca451796b07
Nikhil <nikpawarnp2@gmail.com>
2023-07-13 10:52:33 +0530

    Arraylist class added

---
+++ src/com/amdocs/jp/insurance/ArrayListt.java
@@ -0,0 +1,21 @@
+package com.amdocs.jp.insurance;
+
+import java.util.ArrayList;
+
+public class ArrayListt 
+{
+
+	public static void main(String[] args) {
+		
+		ArrayList arrl = new ArrayList();
+		
+		arrl.add(89);
+		arrl.add("nikhil");
+		arrl.add(null);
+		arrl.add(67);
+		
+		
+		System.out.println(arrl);
+	}
+	
+}
